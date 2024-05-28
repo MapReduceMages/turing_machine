d364465c7fe9af41b9b00095fcea77f06a7f8e79
Selfsigned <me@selfsigned.dev>
2024-05-28 15:15:56 +0200

    Functional turing machine that doesn't fully work (yet)

--- main.ts
+++ main.ts
@@ -1,5 +1,29 @@
-import immutableTest from "./src/lib/logic/immutable_test";
-import test_machine from "./src/lib/logic/machine";
+import {run} from './src/lib/logic/machine.ts';
+import type {TransitionMap} from './src/lib/logic/machine.ts';
 
-console.log("immutable_test:", immutableTest());
-console.log("test_machine:", test_machine())
+const inputTransitions: TransitionMap = {
+    "scanright": {
+        '.': {write: ".", toState: "scanright", action: "RIGHT"},
+        '1': {write: "1", toState: "scanright", action: "RIGHT"},
+        '-': {write: "-", toState: "scanright", action: "RIGHT"},
+        '=': {write: "=", toState: "eraseone", action: "LEFT"},
+    }, 
+    "eraseone": {
+        '1': {write: "=", toState: "subone", action: "LEFT"},
+        '-': {write: ".", toState: "HALT", action: "LEFT"},
+    },
+    "subone": {
+        '1': {write: "1", toState: "subone", action: "LEFT"},
+        '-': {write: "-", toState: "skip", action: "LEFT"},
+    },
+    "skip": {
+        ".": {write: ".", toState: "skip", action: "LEFT"},
+        "1": {write: ".", toState: "scanright", action: "RIGHT"},
+    }
+};
+
+try {
+    run ({tape: "111-11=".split(''), headPosition: 0, currentState: "scanright"})(inputTransitions); 
+} catch (error: any) {
+    console.error(error.message);
+}
--- src/lib/logic/machine.ts
+++ src/lib/logic/machine.ts
@@ -1,11 +1,44 @@
-import * as immutable from 'immutable';
+// consts, will need to be decoupled partly
+const alphabet = ['1', '.', '-', '='];
+const blank = '.';
+const states = ['scanright', 'eraseone', 'subone', 'skip', 'HALT'];
+const finals = ['HALT'];
 
-export default function test_machine() {
-    const TAPE_INITIAL_SIZE = 20;
-    const blank_character = '.';
-
-    const tapeCells = immutable.List<number>(Array(TAPE_INITIAL_SIZE).fill(blank_character));
-    console.log(tapeCells.values());
-    tapeCells.concat(immutable.List<number>(Array(10).fill(0)));
-    console.log(tapeCells.values());
+type State = string;
+export type Direction = 'LEFT' | 'RIGHT';
+export type Transition = {
+    readonly write: string;
+    readonly toState: State;
+    readonly action: Direction;
 }
+
+export interface Machine {
+    readonly tape: Array<string>;
+    readonly headPosition: number;
+    readonly currentState: State;
+}
+
+export type TransitionMap = Record<State, Record<string, Transition>>;
+
+
+function visualize(machine: Machine, transitions : TransitionMap) {
+    machine.tape[machine.headPosition] = '<' + machine.tape[machine.headPosition] + '>';
+    console.log('[' + machine.tape + ']', machine.currentState);
+}
+
+
+const moveHead = (headposition: number) => (direction: Direction) => direction === "LEFT" ? headposition - 1 : headposition + 1;
+const step = (machine: Machine) => (transitions : TransitionMap) : Machine => {
+    // DEBUG, with side effects!
+    visualize(structuredClone(machine), structuredClone(transitions));
+
+    const symbol = machine.tape[machine.headPosition];
+    const transition = transitions[machine.currentState][symbol];
+    return {
+        tape: [...machine.tape.slice(0, machine.headPosition), transition.write, ...machine.tape.slice(machine.headPosition + 1)],
+        headPosition: moveHead(machine.headPosition)(transition.action),
+        currentState: transition.toState
+    };
+};
+const shouldHalt = (currentState : State) => finals.includes(currentState);
+export const run = (machine: Machine) => (transitions : TransitionMap ): Machine => shouldHalt(machine.currentState) ? machine : run(step(machine)(transitions))(transitions);
